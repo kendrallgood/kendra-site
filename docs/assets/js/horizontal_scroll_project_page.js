ba8e2c790185745391bed216d04bb4aff237a65c
sarah <sarahjadepratt@gmail.com>
2020-09-02 18:52:52 -0700

    updating projects/project pages

--- docs/assets/js/horizontal_scroll_project_page.js
+++ docs/assets/js/horizontal_scroll_project_page.js
@@ -1,36 +1,57 @@
-function elementInViewport(i) {
-    var bounding = i.getBoundingClientRect();
-    var myElementHeight = i.offsetHeight;
-    var myElementWidth = i.offsetWidth;
+---
+# front matter stuff
+---
 
-    if (bounding.top >= -myElementHeight 
-        && bounding.left >= -myElementWidth
-        && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
-        && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function elementInViewport(i) {
+//     var bounding = i.getBoundingClientRect();
+//     var myElementHeight = i.offsetHeight;
+//     var myElementWidth = i.offsetWidth;
+
+//     if (bounding.top >= -myElementHeight 
+//         && bounding.left >= -myElementWidth
+//         && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
+//         && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
 $(document).ready(function () {
+    // add image description into 
     var images = $('.project-image');
-    
     var image_container = $('#images_container');
-  
-    for (var scroll_obj of scroll_objs) {
-        if (elementInViewport(scroll_obj)) {
-            $(scroll_obj).animate({ 'opacity': '1' }, 1000);
-        } else {
-            hidden_scroll_objs.push($(scroll_obj));
-        }
+    // image_container.children(":first");
+    
+    var options = {
+      sectionSelector: '.project-image',
+      targetSelector: '.nav-link',
+      offset: 0, // in pixels
+      hrefAttribute: 'href',
+      activeClass: 'actives',
     }
+    // scrollSpy('nav', {
+    //   sectionSelector: '.project-image',
+    //   targetSelector: '.nav-link',
+    //   offset: 0, // in pixels
+    //   hrefAttribute: 'href',
+    //   activeClass: 'active',
+    // });
 
-    $(window).scroll(function () {
-        for (var h_scroll_obj of hidden_scroll_objs) {
-          if (elementInViewport(h_scroll_obj[0])) {
-            $(h_scroll_obj).animate({ 'opacity': '1' }, 1000);
-          }
-        }
-    });
+    scrollSpy(document.getElementById('nav-div'), options);
+    // for (var scroll_obj of scroll_objs) {
+    //     if (elementInViewport(scroll_obj)) {
+    //         $(scroll_obj).animate({ 'opacity': '1' }, 1000);
+    //     } else {
+    //         hidden_scroll_objs.push($(scroll_obj));
+    //     }
+    // }
+
+    // $(window).scroll(function () {
+    //     for (var h_scroll_obj of hidden_scroll_objs) {
+    //       if (elementInViewport(h_scroll_obj[0])) {
+    //         $(h_scroll_obj).animate({ 'opacity': '1' }, 1000);
+    //       }
+    //     }
+    // });
 });
